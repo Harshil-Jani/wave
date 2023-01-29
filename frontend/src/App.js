7c989eb01e2bdafb8ae4c40c1767beb8db3c9367
Harshil Jani <harshiljani2002@gmail.com>
2023-01-29 15:14:12 +0530

    remove Submodule
    Signed-off-by: Harshil Jani <harshiljani2002@gmail.com>

---
+++ frontend/src/App.js
@@ -0,0 +1,137 @@
+import { useEffect, useState } from "react";
+import { ethers } from "ethers";
+import './App.css';
+
+import abi from "./utils/wave.json";
+
+const getEthereumObject = () => window.ethereum;
+const findMetaMaskAccount = async () => {
+  try {
+    const ethereum = getEthereumObject();
+
+    /*
+    * First make sure we have access to the Ethereum object.
+    */
+    if (!ethereum) {
+      console.error("Make sure you have Metamask!");
+      return null;
+    }
+
+    console.log("We have the Ethereum object", ethereum);
+    const accounts = await ethereum.request({ method: "eth_accounts" });
+
+    if (accounts.length !== 0) {
+      const account = accounts[0];
+      console.log("Found an authorized account:", account);
+      return account;
+    } else {
+      console.error("No authorized account found");
+      return null;
+    }
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
+
+export default function App() {
+  const [currentAccount, setCurrentAccount] = useState("");
+  const [allWaves, setAllWaves] = useState([]);
+  useEffect(() => {
+    findMetaMaskAccount().then((account) => {
+      if (account !== null) {
+        setCurrentAccount(account);
+      }
+    });
+  }, [])
+
+  const connectWallet = async () => {
+    try {
+      const ethereum = getEthereumObject();
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+      getAllWaves();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const wave = async () => {
+    const { ethereum } = window;
+    if (ethereum) {
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const signer = provider.getSigner();
+      const waveContract = new ethers.Contract("0xcc03af26fef0403c75fafc1cf973c1594e1ad153", abi.abi, signer);
+      let count = await waveContract.getTotalWaves();
+      console.log(count.toNumber());
+      const waveTxn = await waveContract.wave("Rant");
+      console.log("Mining...", waveTxn.hash);
+
+      await waveTxn.wait();
+      console.log("Mined -- ", waveTxn.hash);
+
+      count = await waveContract.getTotalWaves();
+      console.log("Retrieved total wave count...", count.toNumber());
+      
+    }
+  }
+
+  const getAllWaves = async () => {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const wavePortalContract = new ethers.Contract('0xcc03af26fef0403c75fafc1cf973c1594e1ad153', abi.abi, signer);
+        const waves = await wavePortalContract.getAllWaves();
+
+        let wavesCleaned = [];
+        waves.forEach(wave => {
+          wavesCleaned.push({
+            address: wave.addr,
+            timestamp: new Date(wave.timestamp * 1000),
+            message: wave.message
+          });
+        });
+        setAllWaves(wavesCleaned);
+        console.log("Harshil",allWaves);
+      } 
+    
+  }
+  return (
+    <div className="mainContainer">
+
+      <div className="dataContainer">
+        <div className="header">
+          👋 Hey there!
+        </div>
+
+        <div className="bio">
+          I am Harshil and I love rants about technologies and managers. Connect your Ethereum wallet and leave a rant for us!
+        </div>
+
+        <button className="waveButton" onClick={wave}>
+          Wave at Me
+        </button>
+        {!currentAccount && (
+          <button className="waveButton" onClick={connectWallet}>
+            Connect Wallet
+          </button>
+        )}
+        {allWaves.map((wave, index) => {
+          return (
+            <div key={index} style={{ backgroundColor: "OldLace", marginTop: "16px", padding: "8px" }}>
+              <div>Address: {wave.address}</div>
+              <div>Time: {wave.timestamp.toString()}</div>
+              <div>Message: {wave.message}</div>
+            </div>)
+        })}
+      </div>
+    </div>
+  );
+}
